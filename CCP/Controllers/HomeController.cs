ddd1e194b25affa0b545a5ffb47728963ef3868f
Abdulaziz <40052862+Abdukhalil-zoda@users.noreply.github.com>
2022-01-22 14:48:24 +0500

    Добавьте файлы проекта.

---
+++ CCP/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CCP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult Main()
+        {
+            return File(System.IO.File.Open(Path.Combine("Assets", "index.html"), FileMode.Open), "text/html");
+        }
+
+        [HttpPost("/login.php")]
+        public IActionResult Login(login login)
+        {
+            var body = HttpContext.Request.Body;
+            return Ok();
+        }
+        
+    }
+}
---
+++ CCP/login.cs
@@ -0,0 +1,8 @@
+﻿namespace CCP
+{
+    public class login
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
